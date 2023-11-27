9ea6f64776b30d9fcd8069b702a8743770ded9a9
rickyxyz <56119701+rickyxyz@users.noreply.github.com>
2023-11-27 20:50:36 +0700

    initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,22 @@
+import "./style.css";
+import TextEditor from "./TextEditor";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import { v4 as uuidv4 } from "uuid";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Navigate to={`/documents/${uuidv4()}`} />} />
+        <Route path="/documents/:id" element={<TextEditor />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
